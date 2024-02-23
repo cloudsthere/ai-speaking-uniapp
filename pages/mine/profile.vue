ae42b192a6d0bbe8f460fe5fee0a9cd594d0a2cd
liang <cloudsthere@163.com>
2024-02-23 08:44:23 +0000

    优化英文名

--- pages/mine/profile.vue
+++ pages/mine/profile.vue
@@ -9,18 +9,22 @@
 			<view class="">英文名</view>
 			<input type="text" :value="en_name" name="en_name" class="weui-input" placeholder="请输入英文名" />
 			<view class="flex gap-3">
-				<view class="" v-for="en_name in en_names" @click="selectEnName(en_name)" >{{en_name}}</view>
+				<view class="" v-for="en_name in en_names">
+					<view class="" @click="selectEnName(en_name.name)">{{en_name.name}}</view>
+					<image @click="play(en_name)" class="w-32" src="/static/icon-voice-grey.svg" />
+				</view>
 			</view>
+			<button @click="getEnNames">换一批</button>
 			<view class="">性别</view>
 			<radio-group name="gender">
-				<label>
-					<radio :value="1" :checked="gender == 1" /><text>男</text>
+				<label @click="selectGender(1)" >
+					<radio :value="1" :checked="gender == 1"  /><text>男</text>
 				</label>
-				<label>
-					<radio :value="0" :checked="gender == 0" /><text>女</text>
+				<label @click="selectGender(0)" >
+					<radio :value="0" :checked="gender == 0"  /><text>女</text>
 				</label>
-				<label>
-					<radio :value="2" :checked="gender == 2" /><text>未知</text>
+				<label @click="selectGender(2)" >
+					<radio :value="2" :checked="gender == 2"  /><text>未知</text>
 				</label>
 			</radio-group>
 			<button form-type="submit">提交</button>
@@ -30,6 +34,7 @@
 
 <script>
 	import utils from '@/common/utils.js';
+	import player from '@/common/player.js';
 
 	export default {
 		data() {
@@ -49,15 +54,30 @@
 				that.nickname = res.user.name
 				that.en_name = res.user.en_name
 				that.gender = res.user.gender
-				
+
 				that.getEnNames()
 			})
 		},
 		methods: {
+			selectEnName(en_name) {
+				console.log(en_name)
+				this.en_name = en_name
+			},
+			selectGender(gender) {
+				console.log('select', gender)
+				this.gender = gender
+			},
 			getEnNames() {
-				utils.request('GET', '/api/en-names', {gender: this.gender}, (res) => {
-					console.log(res)
+				var that = this
+				utils.request('GET', '/api/en-names', {
+					gender: this.gender
+				}, (res) => {
+					// console.log(res)
+					that.en_names = res.en_names;
 				})
+			},
+			play(cd) {
+				player.play(cd)
 			},
 			onChooseAvatar(e) {
 				const {
@@ -76,17 +96,26 @@
 					})
 					return
 				}
-				
+
 				let en_name = e.detail.value.en_name
 				if (!en_name) {
 					uni.showToast({
-						title: '昵称不能为空'
+						title: '英文名不能为空',
+						icon: 'error'
 					})
 					return
 				}
 				
+				if (!/^[0-9a-zA-Z\_]+$/.test(en_name)) {
+					uni.showToast({
+						title: '英文名不规范',
+						icon: 'error'
+					})
+					return
+				}
+
 				let gender = e.detail.value.gender
-				
+
 				let avatar = this.avatarUrl;
 				if (avatar.indexOf('http://tmp') == 0) {
 					avatar = uni.getFileSystemManager().readFileSync(this.avatarUrl, 'base64')
